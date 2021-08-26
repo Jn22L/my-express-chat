6261cab417ff99fa5a6501fea447ae08dd3e31af
jn22l <peaceofjn@gmail.com>
2021-08-26 16:30:08 +0900

    position:fixed설정시 canvas좌표구하기 수정

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -9,6 +9,8 @@ var pos = {
 var canvas, ctx;
 
 function initDraw(event) {
+  console.log("draw event 이벤트", event);
+
   ctx.beginPath();
   pos.drawable = true;
   var coors = getPosition(event);
@@ -50,8 +52,13 @@ function finishDraw() {
 }
 
 function getPosition(event) {
-  var x = event.pageX - canvas.offsetLeft;
-  var y = event.pageY - canvas.offsetTop;
+  // css 에서 position:fixed 설정시 좌표값 부정확.
+  // var x = event.pageX - canvas.offsetLeft;
+  // var y = event.pageY - canvas.offsetTop;
+
+  // position:fixed 정확하게 가져오도록 수정.
+  var x = event.layerX;
+  var y = event.layerY;
   return { X: x, Y: y };
 }
 
