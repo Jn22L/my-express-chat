27fd259c483d3f29f7381623ec339b48a98f788a
jn22l <peaceofjn@gmail.com>
2021-08-27 22:12:37 +0900

    .

--- static/js/index.js
+++ static/js/index.js
@@ -106,6 +106,8 @@ function deviceCheck() {
   // 디바이스 종류 설정
   var pcDevice = "win16|win32|win64|mac|macintel";
 
+  document.getElementById("input_send").value = "deviceCheck...resize!";
+
   // 접속한 디바이스 환경
   if (navigator.platform) {
     if (pcDevice.indexOf(navigator.platform.toLowerCase()) < 0) {
